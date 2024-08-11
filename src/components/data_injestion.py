784656ed433202d84972192bd5c51e75025f3ce4
sushank99 <sushank979@gmail.com>
2024-08-11 10:35:21 +0530

    Data injestion

---
+++ src/components/data_injestion.py
@@ -0,0 +1,46 @@
+import os 
+import sys
+
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from logger import logging
+import pandas as pd
+from exception import CustomException
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifact',"train.csv")
+    test_data_path:str = os.path.join('artifact',"test.csv")
+    raw_data_path:str = os.path.join('artifact',"raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion method or component")
+        try:
+            df = pd.read_csv(r'C:\Users\susha\PycharmProjects\MLproject\notebook\data\stud.csv')
+            logging.info('reading the dataset')
+                
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info('train and test split started')
+
+            train_set, test_set= train_test_split(df, test_size = 0.2, random_state = 42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)        
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)        
+            
+            logging.info('ingestion of the data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+                )
+        except Exception as e:
+            raise CustomException(e,sys)
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-from src.logger import logging
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from logger import logging
 
 def error_message_details(error, error_detail:sys):  
     _,_,exc_tb = error_detail.exc_info()
