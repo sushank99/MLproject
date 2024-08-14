47dd83832e3dfe9e71bf3162ee7ef6d31b07cddb
sushank99 <sushank979@gmail.com>
2024-08-14 15:44:09 +0530

    AWS Deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -13,10 +13,10 @@ class PredictPipeline:
         try:
             model_path = os.path.join("artifact", "model.pkl")
             preprocessor_path = os.path.join("artifact", "preprocessing.pkl")
-            print("Before Loading")
+
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
-            print("After Loading")
+
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
