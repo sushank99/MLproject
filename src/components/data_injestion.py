da0c4d12d055c69d79081d75541efca3374d4b89
sushank99 <sushank979@gmail.com>
2024-08-21 15:48:00 +0530

    update

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -37,7 +37,6 @@ class DataIngestion:
             train_set, test_set= train_test_split(df, test_size = 0.2, random_state = 42)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)        
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)        
-            
             logging.info('ingestion of the data is completed')
 
             return(
